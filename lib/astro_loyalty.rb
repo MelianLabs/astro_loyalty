f0188f5e060baaac1e50513f4322a973c77c9f74
Chris Atanasian <catanasian@gmail.com>
2025-05-22 11:53:29 -0700

    Rubocop

--- lib/astro_loyalty.rb
+++ lib/astro_loyalty.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'astro_loyalty/version'
+require 'astro_loyalty/client'
 
 module AstroLoyalty
   class Error < StandardError; end
-  # Your code goes here...
 end
