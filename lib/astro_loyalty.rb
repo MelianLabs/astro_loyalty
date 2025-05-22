21a17d1845f0949f8cfd70034267a8c48e19f57f
Chris Atanasian <catanasian@gmail.com>
2025-05-22 11:18:47 -0700

    Initial commit

---
+++ lib/astro_loyalty.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "astro_loyalty/version"
+
+module AstroLoyalty
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/astro_loyalty/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AstroLoyalty
+  VERSION = "0.1.0"
+end
